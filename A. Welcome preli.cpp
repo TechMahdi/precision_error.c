568e44a5ff7e2e52c7c4f9e04ab428fc65d5d744
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-05-27 09:39:57 +0600

    Create A. Welcome preli.cpp

---
+++ A. Welcome preli.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+   cout<<"Welcome to ACM ICPC Dhaka Regional Online Preliminary Contest, 2018"<<"\n";
+
+
+
+    return 0;
+}
