2164b1553faf086a4f754150977dc70184b9dd86
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-05-27 21:47:39 +0600

    Create sorting three numbers.cpp

---
+++ sorting three numbers.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int a,b,c,temp;
+    cin>>a>>b>>c;
+    if(a>b)
+    {
+        temp=a;
+        a=b;
+        b=temp;
+
+    }
+     if(a>c)
+    {
+        temp=a;
+        a=c;
+        c=temp;
+
+    }
+     if(b>c)
+    {
+        temp=b;
+        b=c;
+        c=temp;
+
+    }
+    cout<<a<<" "<<b<<" "<<c<<endl;
+
+
+
+    return 0;
+}
