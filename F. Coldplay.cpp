ca7cafeaea9c9c708bcbe3a123f3c948f94d1da4
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-05-27 11:00:38 +0600

    Create F. Coldplay.cpp

---
+++ F. Coldplay.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int p ,q, y,result;
+    cin >> p >> q >> y;
+    result =(5*p+2*q)*52*y;
+    cout << result <<"\n";
+
+
+
+    return 0;
+}
