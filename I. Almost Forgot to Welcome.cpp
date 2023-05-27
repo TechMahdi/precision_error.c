4072d89860c84833e68383ad2a98ed0687f89f14
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-05-27 09:39:03 +0600

    Create I. Almost Forgot to Welcome.cpp

---
+++ I. Almost Forgot to Welcome.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+cout<<"Welcome to ICPC Dhaka Regional Online Preliminary Contest, 2019"<<"\n";
+
+
+
+    return 0;
+}
