43cddeaf091804f93d12543600885fc536ad69bd
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-05-29 23:50:04 +0600

    Update Count k-divisor numbers (I).cpp

--- Count k-divisor numbers (I).cpp
+++ Count k-divisor numbers (I).cpp
@@ -1,47 +1,38 @@
-#include <iostream>
-//#include <vector>
-
+#include<iostream>
 using namespace std;
+int count_divisors(int num)
+{
+    int count =0;
+    for (int k=1;k<=num;k++)
+    {
+        if(num%k==0)
+            count++;
+    }
+    return count ;
 
-// Function to count the number of divisors of a given number
-int countDivisors(int num) {
-    int count = 0;
-    for (int i = 1; i *i <= num; i++) {
-        if (num % i == 0) {
-            // Counting both divisors i and num/i
-            if (i * i == num)
-                count++;
-            else
-                count += 2;
+}
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int l,r,k,t;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        cin>>l>>r>>k;
+        int freq=0;
+        for(int j=l;j<=r;j++)
+        {
+            int divisors=count_divisors(j);
+            if(divisors==k)
+            {
+                freq++;
+            }
         }
+        cout<<"Case"<<" "<<i<<":"<<" "<<freq<<endl;
     }
-    return count;
-}
 
-// Function to find the frequency of numbers between l and r with exactly k divisors
-int findFrequency(int l, int r, int k) {
-    int frequency = 0;
-    for (int i = l; i <= r; i++) {
-        int divisors = countDivisors(i);
-        if (divisors == k)
-            frequency++;
-    }
-    return frequency;
-}
-
-int main() {
-    int T;
-    cin >> T;
-
-    for (int testCase = 1; testCase <= T; testCase++) {
-        int l, r, k;
-        cin >> l >> r >> k;
-
-        int frequency = findFrequency(l, r, k);
-
-        cout << "Case " << testCase << ": " << frequency << endl;
-    }
 
     return 0;
 }
-
