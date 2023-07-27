273468ba66ac4937107b30c7e6eedaef50e72d79
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-07-27 15:15:06 +0600

    Create linear search.cpp

---
+++ linear search.cpp
@@ -0,0 +1,32 @@
+//lineaar seach code
+#include<bits/stdc++.h>
+using namespace std;
+int linear (int arr[ ],int n,int key)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==key)
+        {
+            return i;
+        }
+
+    }
+    return -1;
+}
+int main ()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,key;
+    cin>>n>>key;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<linear(arr,n,key);
+
+    return 0;
+}
+
