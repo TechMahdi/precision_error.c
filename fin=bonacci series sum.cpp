fb808f22bac9e314c40c25044e4e029d08512b55
Mahdi chowdhury 104 <105577478+mahdiGig@users.noreply.github.com>
2023-08-04 20:53:20 +0600

    Update fin=bonacci series sum.cpp

--- fin=bonacci series sum.cpp
+++ fin=bonacci series sum.cpp
@@ -7,19 +7,24 @@ int main ()
     cout.tie(NULL);
     int k;
     cin>>k;
-   int arr[k];
-   int sum=0;
-arr[0]=0;
-arr[1]=1;
+    int arr[k];
+    arr[0]=0;
+    arr[1]=1;
+    int sum=0;
+    for(int i=2;i<=k;i++)
+    {
+        arr[i]=arr[i-1]+arr[i-2];
+    }
+     for(int i=0;i<=k;i++)
+    {
+       cout<< arr[i]<<" ";
+    }
+    cout<<"\n";
+    for(int i=0;i<=k;i++)
+    {
+        sum+=arr[i];
+    }
 
-   for(int i=2;i<=k;i++)
-   {
-       arr[i]=arr[i-1]+arr[i-2];
-   }
-   for(int i=0;i<=k;i++)
-   {
-       sum+=arr[i];
-   }
-cout<<sum;
+cout<<sum<<" ";
     return 0;
 }
